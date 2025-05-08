525818dc95b91f047575a4d9c13b481d0408f92c
valeria <vebriferr@gmail.com>
2025-05-08 09:50:42 +0200

    contenido clases categoria y cliente

--- Reto3ValeriaKristell/src/clases/Categoria.java
+++ Reto3ValeriaKristell/src/clases/Categoria.java
@@ -1,5 +1,32 @@
 package clases;
 
 public class Categoria {
+private int idcategoria;
+private String nombre;
+
+public Categoria(int idcategoria, String nombre) {
+	super();
+	this.idcategoria = idcategoria;
+	this.nombre = nombre;
+}
+
+public int getIdcategoria() {
+	return idcategoria;
+}
+public void setIdcategoria(int idcategoria) {
+	this.idcategoria = idcategoria;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+
+@Override
+public String toString() {
+	return "Categoria: idcategoria=" + idcategoria + ", nombre=" + nombre ;
+}
+
 
 }
--- Reto3ValeriaKristell/src/clases/Cliente.java
+++ Reto3ValeriaKristell/src/clases/Cliente.java
@@ -1,5 +1,37 @@
 package clases;
 
 public class Cliente {
+private int idcliente;
+private String nombre,direccion;
 
+public int getIdcliente() {
+	return idcliente;
+}
+public void setIdcliente(int idcliente) {
+	this.idcliente = idcliente;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+
+public String getDireccion() {
+	return direccion;
+}
+public void setDireccion(String direccion) {
+	this.direccion = direccion;
+}
+
+public Cliente(int idcliente, String nombre, String direccion) {
+	super();
+	this.idcliente = idcliente;
+	this.nombre = nombre;
+	this.direccion = direccion;
+}
+@Override
+public String toString() {
+	return "Cliente: idcliente=" + idcliente + ", nombre=" + nombre + ", direccion=" + direccion ;
+}
 }
