490a265bc1658331722e5ae0f347fa380e773b44
KristellVM <krisvm31@gmail.com>
2025-05-08 09:52:02 +0200

    creada clase producto

---
+++ Reto3ValeriaKristell/src/clases/Producto.java
@@ -0,0 +1,61 @@
+package clases;
+
+public class Producto {
+	private int idProducto;
+	private String nombre;
+	private double precio;
+	private String descripcion;
+	private String color;
+	private String talla;
+	private int stock;
+	
+	public int getIdProducto() {
+		return idProducto;
+	}
+	public void setIdProducto(int idProducto) {
+		this.idProducto = idProducto;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getTalla() {
+		return talla;
+	}
+	public void setTalla(String talla) {
+		this.talla = talla;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	@Override
+	public String toString() {
+		return "Producto [idProducto=" + idProducto + ", nombre=" + nombre + ", precio=" + precio + ", descripcion="
+				+ descripcion + ", color=" + color + ", talla=" + talla + ", stock=" + stock + "]";
+	}
+	
+	
+}
