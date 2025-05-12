1673e7f2ec57ab5bcfa2975f64b8ee0bc5e27b13
valeria <vebriferr@gmail.com>
2025-05-12 13:28:01 +0200

    productoDao

--- Reto3ValeriaKristell/src/dao/ProductoDAO.java
+++ Reto3ValeriaKristell/src/dao/ProductoDAO.java
@@ -11,6 +11,7 @@ import clases.Producto;
 import util.Conexion;
 
 public class ProductoDAO {
+	//FUNCION 1
 	public static List<Producto> FuncionListaProductos(String categoriaNombre)
 	{
 		List<Producto> listaProductos = new ArrayList<Producto>();
@@ -42,4 +43,42 @@ public class ProductoDAO {
 		}
 		return listaProductos;
 	}
+	
+	//FUNCION 2
+	
+	public static List<Producto> FuncionBuscarProductos(String nomProducto,String talla,String color)
+	{
+		List<Producto> listaProductos = new ArrayList<Producto>();
+		try {
+			//abro bd
+			Connection con = Conexion.abreConexion();
+			//creo el statement
+			PreparedStatement pst = con.prepareStatement("\r\n"
+					+ "	select * from productos\r\n"
+					+ "	where productos.nombre=% and talla=% and color=%;\r\n"
+					+ "\r\n"
+					+ "");
+			pst.setString(1,nomProducto);
+			pst.setString(2,talla);
+			pst.setString(3,color);
+			ResultSet rs = pst.executeQuery();
+			while(rs.next())
+			{
+			Categoria categoria =new Categoria(rs.getInt("idcategoria"),rs.getString("nombre"));//int idcategoria, String nombre
+			Producto producto=new Producto(rs.getInt("idProducto"),categoria,rs.getString("nombre"),
+					rs.getDouble("precio"),rs.getString("descripcion"),rs.getString("color"),rs.getString("talla"),
+					rs.getInt("stock"));
+
+			listaProductos.add(producto);
+			}
+			rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			Conexion.cierraConexion();
+		}
+		return listaProductos;
+	}
+	
 }
