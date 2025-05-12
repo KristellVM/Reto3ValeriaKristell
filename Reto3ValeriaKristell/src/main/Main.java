da78f965e1919b2a6a3779f8c7ada0593f8a4ecc
valeria <vebriferr@gmail.com>
2025-05-12 12:30:39 +0200

    main

--- Reto3ValeriaKristell/src/main/Main.java
+++ Reto3ValeriaKristell/src/main/Main.java
@@ -1,5 +1,44 @@
 package main;
 
+import java.util.Scanner;
+
+import util.Funciones;
+
 public class Main {
 
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int opcion;
+		
+		do {
+			opcion = Funciones.dimeEntero("Elige una opcion, selecciona 0 para salir", sc);
+			switch (opcion) {
+			case 1:
+				//FuncionMantenimientos();
+				break;
+
+			case 2:
+				//FuncionCatalogoProductos();
+				break;
+
+			case 3:
+				//FuncionPedidos():
+				break;
+
+			case 4:
+				//FuncionInformes();				
+				break;
+
+			default:
+				break;
+			}
+		} while (opcion != 0);
+
+	}
+	
+	
+	
+	
+	
+	
 }
