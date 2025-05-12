349ec5c91c92b86f77c2d88d1814606e0ff103e4
KristellVM <krisvm31@gmail.com>
2025-05-12 12:39:24 +0200

    categoriaDAO creada

---
+++ Reto3ValeriaKristell/src/dao/CategoriaDAO.java
@@ -0,0 +1,32 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import clases.Categoria;
+import util.Conexion;
+
+public class CategoriaDAO {
+	public static void inserta(Categoria categoria) {
+		try {
+			//abro conexion
+			Connection con=Conexion.abreConexion();
+			//creo select
+			PreparedStatement pst=con.prepareStatement("INSERT into categorias(idcategoria, nombre) values(?,?);");
+			pst.setInt(1, categoria.getIdcategoria());
+			pst.setString(2, categoria.getNombre());
+			pst.execute();
+			ResultSet rs = pst.getGeneratedKeys();
+			if(rs.next()) {
+			}
+			
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			finally {//O en el try abrir la conexion
+				Conexion.cierraConexion();
+			}
+	}
+}
