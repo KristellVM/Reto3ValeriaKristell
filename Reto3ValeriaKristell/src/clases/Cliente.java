7e0526a3a9b5fbbcae37f45d74f8c9bec3449a17
valeria <vebriferr@gmail.com>
2025-05-12 12:52:15 +0200

    clase cliente

--- Reto3ValeriaKristell/src/clases/Cliente.java
+++ Reto3ValeriaKristell/src/clases/Cliente.java
@@ -1,7 +1,7 @@
 package clases;
 
 public class Cliente {
-private int idcliente;
+private int idcliente,codigo;
 private String nombre,direccion;
 
 public int getIdcliente() {
@@ -10,13 +10,18 @@ public int getIdcliente() {
 public void setIdcliente(int idcliente) {
 	this.idcliente = idcliente;
 }
+public int getCodigo() {
+	return codigo;
+}
+public void setCodigo(int codigo) {
+	this.codigo = codigo;
+}
 public String getNombre() {
 	return nombre;
 }
 public void setNombre(String nombre) {
 	this.nombre = nombre;
 }
-
 public String getDireccion() {
 	return direccion;
 }
@@ -24,14 +29,18 @@ public void setDireccion(String direccion) {
 	this.direccion = direccion;
 }
 
-public Cliente(int idcliente, String nombre, String direccion) {
+
+public Cliente(int idcliente, int codigo, String nombre, String direccion) {
 	super();
 	this.idcliente = idcliente;
+	this.codigo = codigo;
 	this.nombre = nombre;
 	this.direccion = direccion;
 }
 @Override
 public String toString() {
-	return "Cliente: idcliente=" + idcliente + ", nombre=" + nombre + ", direccion=" + direccion ;
+	return "idcliente=" + idcliente + ", codigo=" + codigo + ", nombre=" + nombre + ", direccion=" + direccion
+			;
 }
+
 }
