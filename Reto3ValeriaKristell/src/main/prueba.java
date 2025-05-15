79940c6dd9646f9ce84b5c8f5b773634ffd7a8e0
valeria <vebriferr@gmail.com>
2025-05-15 08:30:17 +0200

    Merge branch 'main' of https://github.com/KristellVM/Reto3ValeriaKristell.git into main

--- Reto3ValeriaKristell/src/main/prueba.java
+++ Reto3ValeriaKristell/src/main/prueba.java
@@ -1,8 +1,12 @@
 package main;
 
+import java.util.List;
 import java.util.Scanner;
 
 import clases.Cliente;
+import clases.Pedido;
+import clases.PedidoProducto;
+import clases.Producto;
 import dao.ClienteDAO;
 import dao.PedidosDAO;
 import dao.ProductoDAO;
@@ -11,7 +15,12 @@ import util.Funciones;
 public class prueba {
 
 	public static void main(String[] args) {
-	
+		Scanner sc=new Scanner(System.in);
+	System.out.println(PedidosDAO.  verPedidos());
+		
+
 	}
+		
+	
 
 }
