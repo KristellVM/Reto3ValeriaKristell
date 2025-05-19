5336fa24e01fa4887995398f31b2f3bf41205f19
valeria <vebriferr@gmail.com>
2025-05-19 13:56:01 +0200

    prueba

--- Reto3ValeriaKristell/src/main/otraPRUEBA.java
+++ Reto3ValeriaKristell/src/main/otraPRUEBA.java
@@ -2,19 +2,24 @@ package main;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
+import clases.Cliente;
 import clases.Pedido;
+import dao.ClienteDAO;
 import dao.PedidosDAO;
+import util.Funciones;
 
 public class otraPRUEBA {
 
 	public static void main(String[] args) {
-		System.out.println(PedidosDAO.verPedidos());
-
-		List<Pedido> listaPedidos = new ArrayList<Pedido>();
-		for (Pedido pedido : listaPedidos) {
-			System.out.println(pedido);
+		Scanner sc=new Scanner(System.in);
+		int cod = Funciones.dimeEntero("Introduce el codigo de un cliente", sc);
+		Cliente cliente = ClienteDAO.buscar(cod);
+		if (cliente == null) {
+			System.out.println("El cliente no existe");
 		}
+		List<Pedido> listaPedidos = Main4.PedidosPorCliente(cliente);
 
 	}
 }
